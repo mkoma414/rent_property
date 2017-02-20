5158e51a3a15f6b728992e7cbbeceda0eedd4e08
Masato Komatsubara <masa@Masato-no-MacBook.local>
2017-02-20 23:00:26 +0900

    fix update error

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -19,12 +19,11 @@ class PropertiesController < ApplicationController
   end
 
   def edit
-
+    
   end
 
   def show
     @station = @property.stations
-    
   end
 
   def update
@@ -45,8 +44,8 @@ class PropertiesController < ApplicationController
 
   def property_params
     # params.require(:property).permit(:property_name, :fee, :address, :building_age, :remarks)
-      params.require(:property).permit(:property_name, :fee, :address, :building_age, :remarks,
-        stations_attributes: [:line_name, :station_name, :walk_minutes])
+      params.require(:property).permit(:id, :property_name, :fee, :address, :building_age, :remarks,
+        stations_attributes: [:id, :line_name, :station_name, :walk_minutes])
   end
 
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,4 @@
 class Station < ApplicationRecord
   belongs_to :property, optional: true
+  # belongs_to :property, inverse_of: :properties
 end
