185af5c435f73a132b75b71dca94a43ec51e62b7
Masato Komatsubara <masa@Masato-no-MacBook.local>
2017-02-20 18:49:25 +0900

    add CRUD

---
+++ app/controllers/properties_controller.rb
@@ -0,0 +1,46 @@
+class PropertiesController < ApplicationController
+
+  before_action :set_property, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @properties=Property.all
+  end
+
+  def create
+    @property = Property.new(property_params)
+    @property.save
+    redirect_to "/properties"
+  end
+
+  def new
+    @property = Property.new
+  end
+
+  def edit
+
+  end
+
+  def show
+  end
+
+  def update
+    @property.update(property_params)
+    redirect_to "/properties"
+  end
+
+  def destroy
+    @property.destroy
+    redirect_to "/properties"
+  end
+
+  private
+
+    def set_property
+      @property = Property.find(params[:id])
+    end
+
+  def property_params
+    params.require(:property).permit(:property_name, :fee, :address, :building_age, :remarks)
+  end
+
+end
